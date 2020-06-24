f312f7d8d4b05cd6bf5228f0f4bb421637f45206
JAYED <ajayedete@gmail.com>
2020-06-24 10:54:35 +0600

    app.js file updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
+var port = process.env.PORT || 3000;
+    http = require('http');
+    fs = require('fs');
     html = fs.readFileSync('index.html');
 
 var log = function(entry) {
